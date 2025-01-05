00ffbadf8b371756b7017d06ddc70787f0678534
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-01-05 17:33:23 +0700

    logout bloc

---
+++ lib/presentation/auth/bloc/logout_bloc/logout_bloc.dart
@@ -0,0 +1,18 @@
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:komdigi_logbooks_supervisors/data/datasources/auth_remote_datasource.dart';
+
+part 'logout_event.dart';
+part 'logout_state.dart';
+part 'logout_bloc.freezed.dart';
+
+class LogoutBloc extends Bloc<LogoutEvent, LogoutState> {
+  final AuthRemoteDatasource datasource;
+  LogoutBloc(this.datasource) : super(const _Initial()) {
+    on<_Logout>((event, emit) async {
+      emit(const _Loading());
+      final result = await datasource.logout();
+      result.fold((l) => emit(_Error(error: l)), (r) => emit(const _Success()));
+    });
+  }
+}
