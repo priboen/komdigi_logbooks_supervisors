238ef792b59ee1c464555cedfe168df367cad309
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-01-09 04:04:15 +0700

    change baseurl

--- lib/core/constants/variables.dart
+++ lib/core/constants/variables.dart
@@ -1,5 +1,4 @@
 class Variables {
   static const String appName = 'SIMKP Komdigi Admin';
-  // static const String baseUrl = 'http://10.0.2.2:8000';
-  static const String baseUrl = 'http://192.168.1.8:8000';
+  static const String baseUrl = 'http://10.0.2.2:8000';
 }
